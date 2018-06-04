2b63454899478803398b5d0bc31ec1d92837bcba
M L <sapient007@hotmail.com>
2018-06-04 12:36:40 -0400

    Update task.sh

--- tasks/vsphere/nsxv/create-edge/task.sh
+++ tasks/vsphere/nsxv/create-edge/task.sh
@@ -39,13 +39,13 @@ then
   exit 1
 fi
 # Create logical switches
-for labwire_id in $(seq $NUM_LOGICAL_SWITCHES); do
-  echo "***** debugging ********"
-  echo $NSX_EDGE_GEN_NAME
-  echo $OWNER_NAME
-  echo $labwire_id
-  pynsxv_local lswitch -n "labwire-$NSX_EDGE_GEN_NAME-$OWNER_NAME-$labwire_id" create
-done
+#for labwire_id in $(seq $NUM_LOGICAL_SWITCHES); do
+#  echo "***** debugging ********"
+#  echo $NSX_EDGE_GEN_NAME
+#  echo $OWNER_NAME
+#  echo $labwire_id
+#  pynsxv_local lswitch -n "labwire-$NSX_EDGE_GEN_NAME-$OWNER_NAME-$labwire_id" create
+#done
 
 # Create an edge
 echo "****** debugging*******"
